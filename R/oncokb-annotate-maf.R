266f640a7329da4a2f5ac5060dc963b37c0918e1
Bob Zimmermann <robert.paul.zimmermann@gmail.com>
2022-12-15 13:50:50 -0500

    move to chunked queries and 2 other minor features
    - add progress bar ("verbose")
    - add the possibility to shorten queries by distinct queries. It would be the caller's responsibility to join the data frame query outcomes to the results.

--- R/oncokb-annotate-maf.R
+++ R/oncokb-annotate-maf.R
@@ -5,13 +5,13 @@
 #' @param api_token OncoKB API token. This can be retrieved from https://www.oncokb.org/account/settings after you have set up your account.
 #' @param maf Input MAF.
 #' @param cancer_types Data frame with samples mapped to cancer type for accurate levels of actionability.
-#' 
+#'
 #' @param gene Gene.
 #' @param protein_change Abbreviated form, e.g. "H1047R".
 #' @param variant_type Lower case variant type, e.g. "missense".
 #' @param start Genomic start position of variant.
 #' @param end Genomic end position of variant.
-#' @param cancer_type Oncotree code for cancer type. Can be left blank. 
+#' @param cancer_type Oncotree code for cancer type. Can be left blank.
 #' @param parallelize Boolean indicating whether to parallelize annotation, using \code{future} backend.
 #'
 #' @return Annotated MAF with columns indicating functionality of mutation and levels of actionability.
@@ -27,7 +27,7 @@
 #' @importFrom httr modify_url GET content
 #' @importFrom stringr str_replace str_extract
 #' @importFrom tibble tibble add_column
-#' 
+#'
 #' @examples
 #' query_oncokb('PIK3CA', 'H1047R', 'missense')
 #'
@@ -109,7 +109,7 @@ query_oncokb = function(api_token, gene, protein_change, variant_type, cancer_ty
         }
 
         oncokb_response = httr::content(oncokb_response)
-        
+
         cached_entries[[tag]] = oncokb_response
     } else {
         oncokb_response = cached_entries[[tag]]
@@ -135,72 +135,110 @@ query_oncokb = function(api_token, gene, protein_change, variant_type, cancer_ty
   }
 }
 
+
 #' @export
 #' @rdname oncokb_annotate_maf
-mquery_oncokb = function(api_token, gene, protein_change, variant_type, cancer_type = 'CANCER') {
+mquery_oncokb = function(api_token, gene, protein_change, variant_type, cancer_type = 'CANCER', chunk_size = 1000,
+                         shorten_query = F, verbose = F) {
 
   if (any(variant_type != '')) {
-
-    base_url = 'https://www.oncokb.org/api/v1/'
-    info_url = paste0(base_url, "info")
-    query_url = paste0(base_url, "annotate/mutations/byProteinChange")
-    if(!exists('oncokb_version')) {
-      oncokb_version <<- httr::content(httr::GET(info_url))$dataVersion$version
+    query_df <- data.frame(gene, protein_change, variant_type)
+    if(shorten_query) {
+      query_df <- dplyr::distinct(query_df)
+    }
+    query_inputs <- as.list(query_df)
+    if(dplyr::n_distinct(lengths(query_inputs)) != 1) {
+      rlang::abort("Must provide same number of query inputs to mquery_oncokb()")
     }
 
-    query_base <- list(tumor_type = cancer_type,
-                       evidenceTypes = list("ONCOGENIC", "MUTATION_EFFECT",
-                                            "STANDARD_THERAPEUTIC_IMPLICATIONS_FOR_DRUG_SENSITIVITY",
-                                            "INVESTIGATIONAL_THERAPEUTIC_IMPLICATIONS_DRUG_SENSITIVITY",
-                                            "INVESTIGATIONAL_THERAPEUTIC_IMPLICATIONS_DRUG_RESISTANCE"))
-    query_inputs <- list(gene, protein_change, variant_type)
-    body_structure <-  purrr::pmap(
-      query_inputs,
-      ~c(query_base,  list(gene = list(hugoSymbol = ..1),
-                           alteration = ..2, consequence = ..3)))
-    body <- jsonlite::toJSON(body_structure, auto_unbox = TRUE)
+    is <- seq_along(gene)
+    chunks <- split(is, ceiling(is/chunk_size))
 
-    headers <- c(
-      httr::accept_json(),  httr::content_type_json(),
-      httr::add_headers(Authorization = stringr::str_c("Bearer ", api_token)))
-
-    oncokb_response = httr::POST(query_url, headers, body = body)
-
-    if(oncokb_response$headers$`content-type` == "application/problem+json") {
-      json <- httr::content(oncokb_response, "text", "application/json")
-      errmsg <- stringr::str_c("Problem querying oncokb:\n",
-                               jsonlite::prettify(json))
-      rlang::abort(errmsg)
-    }
-
-    oncokb_response = httr::content(oncokb_response)
-
-
-    purrr::map_dfr(oncokb_response,
-                   ~{
-                     drugs = purrr::map(.x$treatments, 'drugs') %>%
-                       purrr::map(., function(x) paste(unlist(x))) %>%
-                       purrr::simplify() %>%
-                       unique()
-
-                     tibble::tibble(
-                       oncogenic = as.character(.x$oncogenic),
-                       oncokb_effect = ifelse(is.null(.x$mutationEffect$knownEffect), '',
-                                              .x$mutationEffect$knownEffect),
-                       oncokb_level = ifelse(is.null(.x$highestSensitiveLevel), '',
-                                             .x$highestSensitiveLevel),
-                       oncokb_resistance_level = ifelse(is.null(.x$highestResistanceLevel), '',
-                                                        .x$highestResistanceLevel),
-                       oncokb_drugs = ifelse(length(drugs) == 0, '',
-                                             paste(unlist(unique(drugs)), collapse = ',')),
-                       oncokb_version = oncokb_version)
-                   })
-
-
+    pb <- progress::progress_bar$new(format = "[:bar] :current/:total chunks eta: :eta", total = length(chunks), force = T)
+    res <-
+      purrr::map_dfr(chunks,
+                     ~{
+                       if(verbose) pb$tick()
+                       mquery_oncokb_chunk(api_token, gene[.x], protein_change[.x],
+                                           variant_type[.x], cancer_type,
+                                           shortened_query = shorten_query)
+                     })
+    pb$terminate()
+    res
   } else {
     tibble::tibble(oncogenic = '')
   }
 }
+
+mquery_oncokb_chunk = function(api_token, gene, protein_change, variant_type, cancer_type = 'CANCER',
+                               shortened_query = F) {
+  base_url = 'https://www.oncokb.org/api/v1/'
+  info_url = paste0(base_url, "info")
+  query_url = paste0(base_url, "annotate/mutations/byProteinChange")
+  if(!exists('oncokb_version')) {
+    oncokb_version <<- httr::content(httr::GET(info_url))$dataVersion$version
+  }
+
+  query_base <- list(tumor_type = cancer_type,
+                     evidenceTypes = list("ONCOGENIC", "MUTATION_EFFECT",
+                                          "STANDARD_THERAPEUTIC_IMPLICATIONS_FOR_DRUG_SENSITIVITY",
+                                          "INVESTIGATIONAL_THERAPEUTIC_IMPLICATIONS_DRUG_SENSITIVITY",
+                                          "INVESTIGATIONAL_THERAPEUTIC_IMPLICATIONS_DRUG_RESISTANCE"))
+  query_inputs <- list(gene, protein_change, variant_type)
+  body_structure <-  purrr::pmap(
+    query_inputs,
+    ~c(query_base,  list(gene = list(hugoSymbol = ..1),
+                         alteration = ..2, consequence = ..3)))
+  body <- jsonlite::toJSON(body_structure, auto_unbox = TRUE)
+
+  headers <- c(
+    httr::accept_json(),  httr::content_type_json(),
+    httr::add_headers(Authorization = stringr::str_c("Bearer ", api_token)))
+
+  oncokb_response = httr::POST(query_url, headers, body = body)
+
+  if(oncokb_response$headers$`content-type` == "application/problem+json") {
+    json <- httr::content(oncokb_response, "text", "application/json")
+    errmsg <- stringr::str_c("Problem querying oncokb:\n",
+                             jsonlite::prettify(json))
+    rlang::abort(errmsg)
+  }
+
+  oncokb_response = httr::content(oncokb_response)
+
+
+  purrr::map_dfr(seq_along(oncokb_response),
+                 ~{
+                   r <- oncokb_response[[.x]]
+                   drugs = purrr::map(r$treatments, 'drugs') %>%
+                     purrr::map(., function(x) paste(unlist(x))) %>%
+                     purrr::simplify() %>%
+                     unique()
+
+                   res <-
+                     tibble::tibble(
+                       oncogenic = as.character(r$oncogenic),
+                       oncokb_effect = ifelse(is.null(r$mutationEffect$knownEffect), '',
+                                              r$mutationEffect$knownEffect),
+                       oncokb_level = ifelse(is.null(r$highestSensitiveLevel), '',
+                                             r$highestSensitiveLevel),
+                       oncokb_resistance_level = ifelse(is.null(r$highestResistanceLevel), '',
+                                                        r$highestResistanceLevel),
+                       oncokb_drugs = ifelse(length(drugs) == 0, '',
+                                             paste(unlist(unique(drugs)), collapse = ',')),
+                       oncokb_version = oncokb_version)
+                   if(shortened_query) {
+                     res <- dplyr::bind_cols(
+                       tibble::tibble(hugoSymbol = gene[.x],
+                                      alteration = protein_change[.x],
+                                      consequence = variant_type[.x]),
+                       res
+                     )
+                   }
+                   res
+                 })
+}
+
 
 #' @export
 #' @rdname oncokb_annotate_maf
@@ -228,14 +266,14 @@ oncokb_annotate_maf = function(api_token, maf, cancer_types = NULL, parallelize 
            start = stringr::str_extract(Protein_position, '^[0-9]+(?=\\/|\\-)'),
            end = stringr::str_extract(Protein_position, '(?<=-)[0-9]+(?=/)')
            ) %>%
-    
         dplyr::select(gene, protein_change, variant_type, cancer_type)
+
     if (parallelize == TRUE) {
         oncokb_cols = furrr::future_pmap_dfr(oncokb_cols, query_oncokb)
     } else {
         oncokb_cols = purrr::pmap_dfr(oncokb_cols, query_oncokb)
     }
-    
+
     dplyr::bind_cols(maf, oncokb_cols)
 
 }
